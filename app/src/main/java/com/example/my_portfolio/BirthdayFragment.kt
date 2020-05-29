18ea9e546730139fd6711c60249e760df250c606
Erin Beachkofski <erbeach527@gmail.com>
2020-05-29 08:49:00 -0500

    Adding birthday easter egg

---
+++ app/src/main/java/com/example/my_portfolio/BirthdayFragment.kt
@@ -0,0 +1,9 @@
+package com.example.my_portfolio
+
+import androidx.fragment.app.Fragment
+
+class BirthdayFragment : Fragment() {
+
+
+
+}
--- app/src/main/java/com/example/my_portfolio/MainActivity.kt
+++ app/src/main/java/com/example/my_portfolio/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.my_portfolio
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.FrameLayout
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentTransaction
 import com.airbnb.lottie.LottieAnimationView
+import java.time.LocalDate
 
 
 class MainActivity : FragmentActivity() {
@@ -14,15 +18,32 @@ class MainActivity : FragmentActivity() {
     lateinit var mFragment: Fragment
     lateinit var animation_my_name_is : LottieAnimationView
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        val fragment = HomeFragment()
-        fragmentTransaction.replace(R.id.container, fragment)
-        fragmentTransaction.commit()
 
+        // ------- BIRTHDAY EASTER EGG -------
+        // If it's my birthday, show cake video on launch screen
+        // If not, go straight to home screen.
+
+        var birthday : String = LocalDate.of(1111, 6,2).toString()
+        var today : String = LocalDate.now().toString()
+
+        birthday = birthday.substring(5)
+        today = today.substring(5)
+
+        if (today.equals(birthday)) {
+            Log.d("GO TO BIRTHDAY FRAGMENT", birthday)
+//            val fragment = BirthdayFragment()
+//            fragmentTransaction.replace(R.id.home_container, fragment)
+        } else {
+            val fragment = HomeFragment()
+            fragmentTransaction.replace(R.id.home_container, fragment)
+        }
+        fragmentTransaction.commit()
     }
 }
